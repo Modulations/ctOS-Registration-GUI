f5be13214414d9142e537767e523eb0783d960e8
Windows10CE <windows10ce@gmail.com>
2018-04-03 03:22:31 -0500

    Profile Saving

--- ctOS_Registration/Form2.cs
+++ ctOS_Registration/Form2.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace ctOS_Registration
 {
@@ -29,6 +31,61 @@ namespace ctOS_Registration
             lisimage.Add(Properties.Resources.ctOS_Background);
             var indexbackimage = DateTime.Now.Second % lisimage.Count;
             this.BackgroundImage = lisimage[indexbackimage];
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            string sterilizeTextBoxText(TextBox boxText)
+            {
+                string text = boxText.ToString();
+
+                return text.Replace("System.Windows.Forms.TextBox, Text: ", "");
+            }
+            string safeFileName(TextBox boxText)
+            {
+                Name = sterilizeTextBoxText(boxText);
+                return Name.Replace(" ", "_");
+            }
+
+            string profilesDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Profiles";
+            string filename = profilesDir + @"\" + safeFileName(nameBox) + @".json";
+
+            try {
+                if (!Directory.Exists(profilesDir)) {
+                    Directory.CreateDirectory(profilesDir);
+                }
+                if (File.Exists(filename)) {
+                    File.Delete(filename);
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } finally {}
+
+            string name = sterilizeTextBoxText(nameBox);
+            string placeOfBirth = sterilizeTextBoxText(birthBox);
+            string age = sterilizeTextBoxText(ageBox);
+            string occupation = sterilizeTextBoxText(occBox);
+            string race = sterilizeTextBoxText(raceBox);
+            string affiliations = sterilizeTextBoxText(affBox);
+            string salary = sterilizeTextBoxText(salBox);
+            string aliases = sterilizeTextBoxText(aliBox);
+            string specs = sterilizeTextBoxText(specBox);
+
+            JObject profile = new JObject(
+                new JProperty("Name", name),
+                new JProperty("Place Of Birth", placeOfBirth),
+                new JProperty("Age", age),
+                new JProperty("Occupation", occupation),
+                new JProperty("Race", race),
+                new JProperty("Affiliations", affiliations),
+                new JProperty("Salary", salary),
+                new JProperty("Aliases", aliases),
+                new JProperty("Specializations", specs));
+
+            try {
+                File.WriteAllText(filename, profile.ToString());
+            }catch(Exception ex) {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }finally {}
         }
     }
 }
